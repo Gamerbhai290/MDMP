ef30471e2c0ff2e55a6d5603ef519331f4517255
Gamerbhai290 <90045075+Gamerbhai290@users.noreply.github.com>
2024-05-31 01:31:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-    const xValues = ["burger",60,70,80,90,100,110,120,130,14,150];
+    const xValues = ["burger",100,70,80,90,100,110,120,130,14,150];
     const yValues = [10,20,60,50,50,10,10,33,42,5, 30];
     
     new Chart("myChart1", {
